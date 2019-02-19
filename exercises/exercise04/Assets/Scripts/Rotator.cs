8eed848f89bc823fe0500f5310dfd173dc7aa44c
Brittany N. Waller <bwaller@gwmail.gwu.edu>
2019-02-19 12:44:52 -0500

    BuildwebUpdate

---
+++ exercises/exercise04/Assets/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+}
