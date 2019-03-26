40d2ee54187fb332ae4246b98186ed692553d355
Brittany N. Waller <bwaller@gwmail.gwu.edu>
2019-03-25 23:29:23 -0400

    Update

--- exercises/exercise07/Assets/Scripts/PlayerController.cs
+++ exercises/exercise07/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    public Text countText;
     public CharacterController characterController;
+    private int count;
 
-    // Start is called before the first frame update
     void Start()
     {
-
+      count = 0;
+      SetCountText ();
     }
 
     // Update is called once per frame
@@ -23,6 +26,21 @@ public class PlayerController : MonoBehaviour
         transform.Rotate(0, Input.GetAxis("Horizontal") * 5, 0);
 
         characterController.Move(new Vector3(0, Input.GetAxis("Vertical") * 2, 0));
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+            if (other.gameObject.CompareTag("Pick Up"))
+            {
+                other.gameObject.SetActive (false);
+                count = count + 1;
+                SetCountText ();
+            }
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString ();
     }
 }
     
